402be7f69784e1d841fb4bd5b1fda3cf2872ecbe
Roger Paniagua <rogerpaniagua@Roger-Paniagua.local>
2026-03-10 15:35:22 -0600

    feat: add sitemap and robots

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://rogerpaniagua.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://rogerpaniagua.com/work/elaniin-la',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://rogerpaniagua.com/work/blue-engine',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
